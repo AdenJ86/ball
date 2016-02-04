e5f461887f68734ac226b322866c2f0eb7e9c76c
AdenJ86 <Wire154LP!G>
2016-02-04 17:33:22 -0500

    horizontal-ball
    works

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 var ball = {
-  x: 20,
-  y: 200,
+  x: 300,
+  y: 380,
   size: 15,
+  direction: -1,
 
   display: function () {
     // note that to access any of ball's properties, you must use dot notation, using the special keyword this
@@ -14,7 +15,7 @@ var ball = {
   },
 
   move: function () {
-    this.x += 1; // this line is the same as: this.x = this.x + 1;
+    this.x += this.direction; // this line is the same as: this.x = this.x + 1;
   },
 
   checkForBounce: function () {
@@ -25,6 +26,7 @@ var ball = {
   bounce: function () {
     // your code goes here; a hint: this will require making your own variable as well as defining this function
     // another hint: notice that ball is defined using object literal notation
+    this.direction = this.direction*-1;
   }
 };
 
